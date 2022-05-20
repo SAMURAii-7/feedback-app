4bfa7d491c5810f0e4b49c6efe693cab9df5aeae
Shubham <shubham.prasad21@gmail.com>
2022-05-20 22:13:27 +0530

    refactor using useContext

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,10 @@
 import FeedbackItem from "./FeedbackItem"
-import PropTypes from 'prop-types'
+import { useContext } from "react"
+import FeedbackContext from "../context/FeedbackContext"
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+    const { feedback } = useContext(FeedbackContext)
+
     if (!feedback || feedback.length === 0) {
         return <p>No feedback yet!</p>
     }
@@ -10,20 +13,9 @@ function FeedbackList({ feedback, handleDelete }) {
         <div className="feedback-list">
             {feedback.map((item) => (
                 <FeedbackItem key={item.id} item={item}
-                    handleDelete={handleDelete}
                 />
             ))}
         </div>
-    )
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired,
-        })
     )
 }
 
